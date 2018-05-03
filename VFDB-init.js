b94e97daaf62164994fb8eded7c9d8e0c69b6661
kenjican <kenjican@163.com>
2018-05-03 08:57:00 +0800

    init VFDB

---
+++ VFDB-init.js
@@ -0,0 +1,29 @@
+const SP = require('serialport');
+const Readline = SP.parsers.Readline;
+let i = 0;
+const U1 = new SP('/dev/VFDB', {
+    baudRate: 38400,
+    dataBits: 7,
+    stopBits: 2,
+    parity: 'none'
+});
+
+const parser = U1.pipe(new Readline({
+    delimiter: '\r\n'
+}));
+
+parser.on('data', function (data) {
+   i++;
+   U1.write();
+
+    }
+});
+
+const cmd =[':0106020A0004E9\r\n',':010602010003F3\r\n',':010602000004F3\r\n',':010609010003EC\r\n']
+
+function init(){
+  U1.write(cmd[i]);
+
+}
+
+U1.on('error', (err) => console.log(err));
